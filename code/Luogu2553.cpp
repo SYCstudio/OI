fa444c70d5f2ce95295c7dbce4e0c0f06571d229
SYCstudio <1726016246@qq.com>
2018-06-21 18:03:03 +0800

    2018.6.21 pm

---
+++ code/Luogu2553.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<algorithm>
+#include<complex>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define mem(Arr,x) memset(Arr,x,sizeof(Arr))
+
+const int maxN=501000;
+const int inf=2147483647;
+
+int n,N;
+char str[maxN];
+int A[maxN],B[maxN];
+complex<ld> P1[maxN],P2[maxN];
+
+int main()
+{
+	while (scanf("%s",str+1)!=EOF)
+	{
+		int len=strlen(str+1);
+		mem(A,0);mem(B,0);
+		for (int i=1,opt=0;i<=len;i++)
+	}
+}
