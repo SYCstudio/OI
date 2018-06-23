a3a9bdbe434d0979d484201f3c11f13b2777985c
SYCstudio <1726016246@qq.com>
2018-06-23 22:32:23 +0800

    2018.6.23 night

--- code/2018.6.23/UOJ34.cpp
+++ code/2018.6.23/UOJ34.cpp
@@ -19,7 +19,7 @@ int Rader[maxN];
 complex<ld> P1[maxN],P2[maxN];
 complex<ld> Wn[maxN],Iwn[maxN];
 
-void FFT(complex<ld> *P,int N,const complex<ld> *W);
+void FFT(complex<ld> *P,int N,const int opt);
 
 int main()
 {
@@ -38,56 +38,24 @@ int main()
 
 	int N,L=0;
 	for (N=1;N<=(n+m+1);N<<=1) L++;
-	//cout<<"N:"<<N<<endl;
 	for (int i=1;i<N;i++) Rader[i]=(Rader[i>>1]>>1)|((i&1)<<(L-1));
-	for (int i=0;i<N;i++) Wn[i]=((complex<ld>)(cos(2*Pi*i/N),sin(2*Pi*i/N))),Iwn[i]=conj(Wn[i]);
-	//for (int i=0;i<N;i++) cout<<Wn[i]<<" ";cout<<endl;
+	for (int i=0;i<N;i++) Wn[i]=complex<ld>(cos(2*Pi*i/N),sin(2*Pi*i/N)),Iwn[i]=conj(Wn[i]);
 
-	//for (int i=0;i<N;i++) cout<<P1[i]<<" ";cout<<endl;
-	//for (int i=0;i<N;i++) cout<<P2[i]<<" ";cout<<endl;
-
-	FFT(P1,N,Wn);FFT(P2,N,Wn);
+	for (int i=0;i<N;i++) cout<<Wn[i]<<" ";cout<<endl;
+	FFT(P1,N,1);FFT(P2,N,1);
 	for (int i=0;i<N;i++) P1[i]=P1[i]*P2[i];
-	
-	//for (int i=0;i<N;i++) cout<<P1[i]<<" ";cout<<endl;
-	//for (int i=0;i<N;i++) cout<<P2[i]<<" ";cout<<endl;
-	
-	FFT(P1,N,Iwn);
-
-	//for (int i=0;i<N;i++) cout<<P1[i]<<" ";cout<<endl;
+	FFT(P1,N,-1);
 	
 	for (int i=0;i<=n+m;i++) printf("%d ",(int)(P1[i].real()/N+0.5));printf("\n");
 	return 0;
 }
-
-
-void FFT(complex<ld> *P,int N,const complex<ld> *W)
+void FFT(complex<ld> *P,int N,const int opt)
 {
 	for (int i=0;i<N;i++) if (i<Rader[i]) swap(P[i],P[Rader[i]]);
 	for (int i=1;i<N;i<<=1)
 	{
 		int l=i<<1;
-		for (int j=0;j<N;j+=l)
-		{
-			for (int k=0;k<i;k++)
-			{
-				complex<ld> X=P[j+k],Y=W[N/l*k]*P[j+k+i];
-				P[j+k]=X+Y;P[j+k+i]=X-Y;
-			}
-		}
-	}
-	//if (opt==-1) for (int i=0;i<N;i++) P[i].real()=P[i].real()/N+0.5;
-	return;
-}
-//*/
-/*
-void FFT(complex<ld> *P,int N,int opt)
-{
-	for (int i=0;i<N;i++) if (i<Rader[i]) swap(P[i],P[Rader[i]]);
-	for (int i=1;i<N;i<<=1)
-	{
 		complex<ld> dw(cos(Pi/i),sin(Pi/i)*opt);
-		int l=i<<1;
 		for (int j=0;j<N;j+=l)
 		{
 			complex<ld> w(1,0);
@@ -98,7 +66,5 @@ void FFT(complex<ld> *P,int N,int opt)
 			}
 		}
 	}
-	if (opt==-1) for (int i=0;i<N;i++) P[i].real()=P[i].real()/N+0.5;
 	return;
 }
-//*/
