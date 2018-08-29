309b423369a19cd879d3bb3fc1062eb3e545e9e1
SYCstudio <1726016246@qq.com>
2018-08-29 12:42:05 +0800

    2018.8.29 noon

--- code/2018.8.29/BZOJ1124.cpp
+++ code/2018.8.29/BZOJ1124.cpp
@@ -15,10 +15,13 @@ const int inf=2147483647;
 int n,Fa[maxN],Dg[maxN],Dgg[maxN];
 bool vis[maxN],alive[maxN];
 queue<int> Q;
+int mxd,cnt,Ans1,Ans2;
+
+void dfs(int u);
 
 int main(){
-	int Ans1=0,Ans2=n;
 	scanf("%d",&n);
+	Ans1=0;Ans2=n;
 	for (int i=1;i<=n;i++){
 		scanf("%d",&Fa[i]);Dg[Fa[i]]++;
 	}
@@ -28,10 +31,34 @@ int main(){
 	while (!Q.empty()){
 		int u=Q.front();Q.pop();
 		int v=Fa[u];
-		if (alive[v]){
-			
+		if (alive[u]){
+			if (alive[v]) Ans1++,alive[v]=0;
+			if (Dg[v]) Dg[v]=0,Q.push(v);
+		}
+		else{
+			if (Dg[v]){
+				Dg[v]--;
+				if (Dg[v]==0) Q.push(v);
+			}
 		}
 	}
+	for (int i=1;i<=n;i++)
+		if ((Dg[i]!=0)&&(vis[i]==0)){
+			mxd=0;cnt=0;
+			dfs(i);
+			if ((mxd==1)&&(cnt>1)) Ans2--;
+		}
+	printf("%d %d\n",Ans1,Ans2);
+	return 0;
+}
+
+void dfs(int u){
+	if (vis[u]) return;
+	vis[u]=1;cnt++;
+	mxd=max(mxd,Dgg[u]);
+	if (alive[u]) alive[Fa[u]]=0,Ans1++;
+	dfs(Fa[u]);
+	return;
 }
 /*
 https://blog.csdn.net/l_0_forever_lf/article/details/78748262
