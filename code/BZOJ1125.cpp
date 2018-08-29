215c2998cedfbc4fed90c465b96d050bf89800bc
SYCstudio <1726016246@qq.com>
2018-08-29 23:54:42 +0800

    2018.8.29 night

--- code/BZOJ1125.cpp
+++ code/BZOJ1125.cpp
@@ -18,7 +18,7 @@ const int inf=2147483647;
 int n,m,L;
 char str[maxN][maxL];
 ull Hash[maxN],Base[maxN];
-int Cnt[maxN];
+int Ans[maxN];
 multiset<ull> Ms;
 
 int main(){
@@ -26,18 +26,26 @@ int main(){
 	scanf("%d%d%d",&n,&L,&m);
 	for (int i=1;i<=n;i++) scanf("%s",str[i]+1);
 	for (int i=1;i<=n;i++){
-		for (int j=1;j<=L;j++) Hash[i]=Hash[i]*base+(ull)(str[j]-'a');
+		for (int j=1;j<=L;j++) Hash[i]=Hash[i]*base+(ull)(str[i][j]-'a');
 		Ms.insert(Hash[i]);
 	}
-
+	for (int i=1;i<=n;i++) Ans[i]=Ms.count(Hash[i]);
+	//cout<<"Input over"<<endl;
+	//for (int i=1;i<=n;i++) cout<<Hash[i]<<" ";cout<<endl;
 	while (m--){
 		int p1,w1,p2,w2;scanf("%d%d%d%d",&p1,&w1,&p2,&w2);
-		Ms.erase(Ms.find(Hash[p1]));Ms.erase(Ms.find(Hash[p2]));
-		Hash[p1]-=str[p1][w1]*Base[L-w1];
-		Hash[p2]-=str[p2][w2]*Base[L-w2];
+		Ms.erase(Ms.find(Hash[p1]));if (p1!=p2) Ms.erase(Ms.find(Hash[p2]));
+		Hash[p1]-=(ull)(str[p1][w1]-'a')*Base[L-w1];
+		Hash[p2]-=(ull)(str[p2][w2]-'a')*Base[L-w2];
 		swap(str[p1][w1],str[p2][w2]);
-		Hash[p1]+=str[p1][w1]*Base[L-w1];
-		Hash[p2]+=str[p2][w2]*Base[L-w2];
-		Ms.insert(Hash[p1]);Ms.insert(Hash[p2]);
+		Hash[p1]+=(ull)(str[p1][w1]-'a')*Base[L-w1];
+		Hash[p2]+=(ull)(str[p2][w2]-'a')*Base[L-w2];
+		Ms.insert(Hash[p1]);if (p1!=p2) Ms.insert(Hash[p2]);
+		for (int i=1;i<=n;i++) Ans[i]=max(Ans[i],(int)Ms.count(Hash[i]));
+		//for (int i=1;i<=n;i++) cout<<Hash[i]<<" ";cout<<endl;
 	}
+
+	for (int i=1;i<=n;i++) printf("%d\n",Ans[i]);
+
+	return 0;
 }
