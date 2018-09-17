47d2fe9dec20a0971e1e4e5502ae9984569dcb77
sycstudio <1726016246@qq.com>
2018-09-17 23:05:20 +0800

    2018.9.17 night

--- code/2018.9.17/BZOJ1495.cpp
+++ code/2018.9.17/BZOJ1495.cpp
@@ -24,16 +24,22 @@ void Solve(int now,int depth);
 int main(){
 	Two[0]=1;for (int i=1;i<maxN;i++) Two[i]=Two[i-1]<<1;
 	scanf("%d",&n);
-	for (int i=1;i<=n;i++) scanf("%d",&O[Two[n]-1+i]);
-	for (int i=1;i<=n;i++) scanf("%d",&C[Two[n]-1+i]);
+	for (int i=1;i<=Two[n];i++) scanf("%d",&O[Two[n]-1+i]);
+	for (int i=1;i<=Two[n];i++) scanf("%d",&C[Two[n]-1+i]);
 	for (int i=1;i<=Two[n];i++)
 		for (int j=i+1;j<=Two[n];j++){
 			int key;scanf("%d",&key);
 			int x=i+Two[n]-1,y=j+Two[n]-1,xx=x,yy=y;
 			while (xx!=yy) xx>>=1,yy>>=1;
-			cout<<i<<" "<<j<<" "<<xx<<endl;
+			//cout<<i<<" "<<j<<" "<<xx<<endl;
 			Cost[xx][x]+=key;Cost[yy][y]+=key;
 		}
+
+	/*
+	for (int i=1;i<=Two[n];i++)
+		for (int j=Two[n];j<=Two[n+1]-1;j++)
+			cout<<"("<<i<<","<<j<<") "<<Cost[i][j]<<endl;
+	//*/
 
 	Solve(1,n);
 
@@ -45,7 +51,7 @@ int main(){
 
 void Solve(int now,int depth){
 	if (depth==0){
-		F[now][O[now]]=0;F[now][O[now]^1]=C[now];
+		F[now][O[now]^1]=0;F[now][O[now]]=C[now];
 		for (int i=now>>1;i!=0;i>>=1) F[now][St[i]]+=Cost[i][now];
 	}
 	else{
@@ -53,13 +59,14 @@ void Solve(int now,int depth){
 		St[now]=0;Solve(lson,depth-1);Solve(rson,depth-1);
 		for (int i=0;i<=Two[depth-1];i++)
 			for (int j=0;j<=Two[depth-1];j++)
-				if (i+j<Two[depth-1]) F[now][i+j]=min(F[now][i+j],F[lson][i]+F[rson][j]);
+				if (i+j>=Two[depth-1]) F[now][i+j]=min(F[now][i+j],F[lson][i]+F[rson][j]);
 		St[now]=1;Solve(lson,depth-1);Solve(rson,depth-1);
 		for (int i=0;i<=Two[depth-1];i++)
 			for (int j=0;j<=Two[depth-1];j++)
-				if (i+j>=Two[depth-1]) F[now][i+j]=min(F[now][i+j],F[lson][i]+F[rson][j]);
+				if (i+j<Two[depth-1]) F[now][i+j]=min(F[now][i+j],F[lson][i]+F[rson][j]);
 	}
-	cout<<"Solve:"<<now<<endl;
-	for (int i=0;i<=Two[depth];i++) cout<<F[now][i]<<" ";cout<<endl;
+	//cout<<"Solve:"<<now<<endl;
+	//for (int i=now>>1;i!=0;i>>=1) cout<<St[i];cout<<endl;
+	//for (int i=0;i<=Two[depth];i++) cout<<F[now][i]<<" ";cout<<endl;
 	return;
 }
