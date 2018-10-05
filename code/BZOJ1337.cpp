d78e61e92a39b3a6567dec50699af1b28093aae0
SYCstudio <1726016246@qq.com>
2018-10-05 23:56:46 +0800

    2018.10.5 night

--- code/BZOJ1337.cpp
+++ code/BZOJ1337.cpp
@@ -7,11 +7,10 @@
 using namespace std;
 
 #define ll long long
-#define ld double
+#define ld long double
 #define mem(Arr,x) memset(Arr,x,sizeof(Arr))
 
 const int maxN=1010000;
-const ld eps=1e-6;
 const int inf=2147483647;
 
 class Point
@@ -28,22 +27,22 @@ ld GetR(Point A,Point B,Point C,Point &O);
 
 int main(){
 	scanf("%d",&n);
-	for (int i=1;i<=n;i++) scanf("%lf%lf",&P[i].x,&P[i].y);
+	for (int i=1;i<=n;i++) scanf("%LF%LF",&P[i].x,&P[i].y);
 	random_shuffle(&P[1],&P[n+1]);
 	Point O=P[1];ld r=0;
 	for (int i=2;i<=n;i++)
-		if (Dist(O,P[i])>r+eps){
+		if (Dist(O,P[i])>r){
 			O=P[i];r=0;
 			for (int j=1;j<i;j++)
-				if (Dist(O,P[j])>r+eps){
+				if (Dist(O,P[j])>r){
 					O=((Point){(P[i].x+P[j].x)/2.0,(P[i].y+P[j].y)/2.0});
 					r=Dist(O,P[j]);
 					for (int k=1;k<j;k++)
-						if (Dist(O,P[k])>r+eps)
+						if (Dist(O,P[k])>r)
 							r=GetR(P[i],P[j],P[k],O);
 				}
 		}
-	printf("%.2lf %.2lf %.2lf\n",O.x,O.y,r);
+	printf("%.3LF\n",r);
 	return 0;
 }
 
