6ab784bc6607490b6abc2b19480fab03abb08d61
SYCstudio <1726016246@qq.com>
2018-10-25 00:01:58 +0800

    2018.10.24 night

--- Practice/2018.10.24/BZOJ3203.cpp
+++ Practice/2018.10.24/BZOJ3203.cpp
@@ -14,29 +14,30 @@ const int inf=2147483647;
 
 int n,d;
 ld X[maxN],Y[maxN];
-int A[maxN],Dist[maxN];
+ll A[maxN],Dist[maxN];
 
 ld Cross(ld x1,ld y1,ld x2,ld y2);
 ld Slope(int id,ld x,ld y);
 
 int main(){
 	scanf("%d%d",&n,&d);
-	for (int i=1;i<=n;i++) scanf("%d%d",&A[i],&Dist[i]),A[i]+=A[i-1];
+	for (int i=1;i<=n;i++) scanf("%lld%lld",&A[i],&Dist[i]),A[i]+=A[i-1];
 	ld Ans=(ld)A[1]/(ld)Dist[1];
-	int top=1;X[1]=d;Y[1]=A[1];
+	int top=1;X[1]=d;Y[1]=0;
 	for (int i=2;i<=n;i++){
-		ld x=(ld)1.0*i*d,y=A[i];
-		while ((top>=2)&&(Cross(x-X[top-1],y-Y[top-1],X[top]-X[top-1],Y[top]-Y[top-1]))>=0) top--;
+		ld x=(ld)1.0*i*d,y=A[i-1];
+		while ((top)&&(Cross(x-X[top-1],y-Y[top-1],X[top]-X[top-1],Y[top]-Y[top-1]))>=0) top--;
 		++top;X[top]=x;Y[top]=y;
-		x=x+Dist[i];
-		int l=2,r=top;
+		x=x+Dist[i];y=A[i];
+		int l=1,r=top,pos=0;
 		do{
 			int mid=(l+r)>>1;
-			if (Slope(mid-1,x,y)<Slope(mid,x,y)) l=mid;
+			if (Slope(mid-1,x,y)<Slope(mid,x,y)) pos=mid,l=mid+1;
 			else r=mid-1;
 		}
-		while (l+1<r);
-		Ans=Ans+Slope(l,x,y);
+		while (l<=r);
+		Ans=Ans+Slope(pos,x,y);
+		//cout<<Slope(l,x,y)<<endl;
 	}
 	printf("%.0LF\n",Ans);return 0;
 }
