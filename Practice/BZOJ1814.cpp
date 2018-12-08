4e62bd990d8def4b7f3a84d73edfdc82de154d60
SYCstudio <1726016246@qq.com>
2018-12-08 18:38:11 +0800

    2018.12.8 pm-night

--- Practice/BZOJ1814.cpp
+++ Practice/BZOJ1814.cpp
@@ -25,43 +25,44 @@ int main(){
 	int up=3;for (int i=1;i<=m;i++) up=up*3;
 	ll Ans=0;
 	F[now][0]=1;
-	cout<<lstx<<" "<<lsty<<endl;
 	for (int i=1;i<=n;i++)
 		for (int j=1;j<=m;j++){
 			now^=1;mem(F[now],0);
 			for (int k=0;k<up;k++)
 				if (F[now^1][k]){
-					int x=k%3,y=k/Pow3[j]%3,key=F[now^1][k];
+					int x=k%3,y=k/Pow3[j-1]%3,key=F[now^1][k];
+					if (j==1) cout<<i-1<<" "<<m<<" "<<k<<" "<<key<<" "<<x<<" "<<y<<endl;
+					else cout<<i<<" "<<j-1<<" "<<k<<" "<<key<<" "<<x<<" "<<y<<endl;
 					if (Mp[i][j]=='.'){
 						if (x==0){
-							if ((y==0)&&(i<n)&&(j<m)) F[now][k+Pow3[j]+1]+=key;
-							if (y==1) F[now][k]+=key,F[now][k-Pow3[j]+1]+=key;
-							if (y==2) F[now][k]+=key,F[now][k-Pow3[j]*2+2]+=key;
+							if ((y==0)&&(i<n)&&(j<m)) F[now][k+Pow3[j-1]+2]+=key;
+							if (y==1) F[now][k]+=key,F[now][k-Pow3[j-1]+1]+=key;
+							if (y==2) F[now][k]+=key,F[now][k-Pow3[j-1]*2+2]+=key;
 						}
 						if (x==1){
-							if (y==0) F[now][k]+=key,F[now][k-1+Pow3[j]]+=key;
+							if (y==0) F[now][k]+=key,F[now][k-1+Pow3[j-1]]+=key;
 							if (y==1){
-								int opt=0,p=j+1;
+								int opt=0,p=j;
 								while (((k/Pow3[p]%3!=2)||opt)&&(p<=m)){
 									if (k/Pow3[p]%3==1) ++opt;
 									else if (k/Pow3[p]%3==2) --opt;
 									++p;
 								}
-								if (p<=m) F[now][k-Pow3[j]-1-Pow3[p]]+=key;
+								if (p<=m) F[now][k-Pow3[j-1]-1-Pow3[p]]+=key;
 							}
 							if ((y==2)&&(i==lstx)&&(j==lsty)) Ans+=key;
 						}
 						if (x==2){
-							if (y==0) F[now][k]+=key,F[now][k-2+Pow3[j]*2]+=key;
-							if (y==1) F[now][k-2-Pow3[j]]+=key;
-							if (y==2){
-								int opt=0,p=j-1;
+							if (y==0) F[now][k]+=key,F[now][k-2+Pow3[j-1]*2]+=key;
+							if (y==1) F[now][k-2-Pow3[j-1]]+=key;
+							if ((y==2)&&(j>=2)){
+								int opt=0,p=j-2;
 								while (((k/Pow3[p]%3!=1)||opt)&&(p>=1)){
 									if (k/Pow3[p]%3==1) --opt;
 									else if (k/Pow3[p]%3==2) ++opt;
 									--p;
 								}
-								if (p>=1) F[now][k-2-Pow3[j]*2+Pow3[p]]+=key;
+								if (p>=1) F[now][k-2-Pow3[j-1]*2+Pow3[p]]+=key;
 							}
 						}
 					}
