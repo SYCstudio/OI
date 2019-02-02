9c2c0cd07687aa661a5ad3bd256f9e11df40418f
SYCstudio <1726016246@qq.com>
2019-02-02 11:55:55 +0800

    2019.2.2 am

--- Practice/2019.2.2/BZOJ2596.cpp
+++ Practice/2019.2.2/BZOJ2596.cpp
@@ -8,7 +8,7 @@
 #include<iostream>
 using namespace std;
 
-#define ld long double
+#define ld double
 #define mem(Arr,x) memset(Arr,x,sizeof(Arr))
 #define sqr(x) ((x)*(x))
 #define mp make_pair
@@ -19,7 +19,7 @@ using namespace std;
 const int maxN=1010;
 const int maxNode=maxN*maxN;
 const int maxM=maxNode*8;
-const ld eps=1e-6;
+const ld eps=1e-7;
 const ld INF=1e18;
 
 class Point{
@@ -33,7 +33,7 @@ public:
 int n,m;ld va,vb,nodecnt=0;
 int edgecnt=0,Head[maxNode],Next[maxM],V[maxM];
 Point P[maxN];
-vector<pair<Point,int> > St[maxN];
+vector<pair<ld,int> > St[maxN];
 ld Dist[maxNode],W[maxM];
 priority_queue<pair<ld,int> > H;
 bool vis[maxNode];
@@ -44,32 +44,34 @@ Point operator + (Point A,Point B);
 Point operator - (Point A,Point B);
 bool operator < (Point A,Point B);
 ostream & operator << (ostream & os,Point A);
+bool cmp(pair<ld,int> A,pair<ld,int> B);
 void Calc(int p,int q1,int q2);
 void Add_Edge(int u,int v,ld w);
 
 int main(){
-    freopen("in","r",stdin);
-    scanf("%d",&n);scanf("%LF%LF",&va,&vb);
+    scanf("%d",&n);scanf("%lf%lf",&va,&vb);
     P[0].x=P[0].y=0;
-    for (int i=1;i<=n;i++) scanf("%LF%LF",&P[i].x,&P[i].y);
-    for (int i=0;i<n;i++) St[i].push_back(mp(P[i],i)),St[i].push_back(mp(P[i+1],i+1));
+    for (int i=1;i<=n;i++) scanf("%lf%lf",&P[i].x,&P[i].y),St[i].push_back(mp((P[i]-P[i-1]).len(),i-1));
     nodecnt=n;mem(Head,-1);
     for (int i=0;i<=n;i++)
-	for (int j=0;j<n;j++)
+	for (int j=0;j<n;j++)////////////////modify
 	    if (i!=j&&i!=j+1){
 		Calc(i,j,j+1);
 		Calc(i,j+1,j);
 	    }
-    cout<<"nodecnt:"<<nodecnt<<endl;
+    //cout<<"nodecnt:"<<nodecnt<<endl;
 
-    for (int i=0;i<n;i++){
-	sort(St[i].begin(),St[i].end());
-	for (int j=1,sz=St[i].size();j<sz;j++)
-	    Add_Edge(St[i][j-1].sd,St[i][j].sd,(St[i][j].ft-St[i][j-1].ft).len()/va);
+    for (int i=1;i<=n;i++){////////////////modify
+	sort(St[i].begin(),St[i].end(),cmp);
+	int sz=St[i].size()-1;
+	for (int j=0;j<sz;j++)
+	    Add_Edge(St[i][j].sd,St[i][j+1].sd,(St[i][j].ft-St[i][j+1].ft)/va);
+	Add_Edge(St[i][sz].sd,i,(St[i][sz].ft)/va);
     }
+    //cout<<"edgecnt:"<<edgecnt<<endl;
 
-    
-    for (int i=0;i<n;i++){
+    /*
+    for (int i=1;i<=1;i++){////////////////modify
 	cout<<i<<":";
 	for (int j=0,sz=St[i].size();j<sz;j++)
 	    cout<<St[i][j].ft<<" ";
@@ -91,13 +93,16 @@ int main(){
 	    }
 	}
     }
-    printf("%.6LF\n",Dist[n]);return 0;
+    printf("%.6lf\n",Dist[n]);return 0;
 }
 ld crs(Point A,Point B){
     return A.x*B.y-A.y*B.x;
 }
 ld dst(Point A,Point B){
     return sqrt(sqr(A.x-B.x)+sqr(A.y-B.y));
+}
+ld dot(Point A,Point B){
+    return A.x*B.x+A.y*B.y;
 }
 Point operator + (Point A,Point B){
     return ((Point){A.x+B.x,A.y+B.y});
@@ -113,38 +118,43 @@ ostream & operator << (ostream & os,Point A){
     os<<"("<<A.x<<","<<A.y<<")";
     return os;
 }
+bool cmp(pair<ld,int> A,pair<ld,int> B){
+    if (fabs(A.ft-B.ft)>eps) return A.ft>B.ft;
+    return A.sd<B.sd;
+}
 void Calc(int p,int q1,int q2){
-    //cout<<"Calc:"<<p<<" "<<q1<<" "<<q2<<endl;
+    //cout<<"Calc:"<<p<<" "<<q1<<" "<<q2<<" "<<P[p]<<" "<<P[q1]<<" "<<P[q2]<<endl;
+    //cout<<"dot:"<<dot(P[q1]-P[q2],P[p]-P[q2])<<endl;
+    if (dot(P[q1]-P[q2],P[p]-P[q2])<eps){
+	Add_Edge(p,q2,(P[p]-P[q2]).len()/vb);
+	return;
+    }
     ld dis=(P[q1]-P[q2]).len();
     ld h=fabs(crs(P[p]-P[q1],P[p]-P[q2]))/dis;
-    if (sqr((P[p]-P[q1]).len())<h*h) return;
-    ld L=sqrt(sqr((P[p]-P[q1]).len())-h*h);
-    //cout<<dis<<" "<<h<<" "<<L<<endl;
-    ld l=0,r=L;
-    while (l+eps<r){
+    ld L=sqrt(sqr((P[p]-P[q2]).len())-h*h+eps);
+    ld l=max((ld)0,L-dis),r=L;
+    if (l>r) return;
+    int cnt=30;
+    while (l+eps<r&&cnt--){
 	ld ql=l+(r-l)/3,qr=r-(r-l)/3;
-	ld lk=ql/va+sqrt(sqr(L-ql)+h*h)/vb,rk=qr/va+sqrt(sqr(L-qr)+h*h)/vb;
+	ld lk=sqrt(ql*ql+h*h)/vb+(L-ql)/va,rk=sqrt(qr*qr+h*h)/vb+(L-qr)/va;
 	if (lk<rk) r=qr;
 	else l=ql;
     }
-    //cout<<"after dv:"<<l<<endl;
-    if (fabs(l)<eps){
-	Add_Edge(p,q1,sqrt(h*h+L*L)/vb);
+    //cout<<dis<<" "<<h<<" "<<L<<" "<<l<<endl;
+    if (fabs(l-L)<eps){
+	Add_Edge(p,q2,sqrt(l*l+h*h)/vb);
 	return;
     }
-    //cout<<(P[q2].x-P[q1].x)/dis*l<<" "<<(P[q2].y-P[q1].y)/dis*l<<endl;
-    ld px=P[q1].x+(P[q2].x-P[q1].x)/dis*l,py=P[q1].y+(P[q2].y-P[q1].y)/dis*l;
-    cout<<"Calc:"<<p<<" "<<q1<<" "<<q2<<endl;
-    cout<<dis<<" "<<h<<" "<<L<<endl;
-    cout<<P[q2].x-P[q1].x<<" "<<P[q2].y-P[q1].y<<endl;
-    cout<<px<<" "<<py<<endl<<endl;
-    St[min(q1,q2)].push_back(make_pair((Point){px,py},++nodecnt));
-    //cout<<"w:"<<sqrt(sqr(L-l)+h*h)/vb<<" "<<vb<<endl;
-    Add_Edge(p,nodecnt,sqrt(sqr(L-l)+h*h)/vb);
+    if (L>dis&&fabs(L-dis-l)<eps){
+	Add_Edge(p,q1,sqrt(l*l+h*h)/vb);
+	return;
+    }
+    q2=max(q1,q2);St[q2].push_back(mp(q1<q2?L-l:dis-(L-l),++nodecnt));
+    Add_Edge(p,nodecnt,sqrt(l*l+h*h)/vb);
     return;
 }
 void Add_Edge(int u,int v,ld w){
-    //cout<<"Add Edge:"<<u<<" "<<v<<" "<<w<<endl;
     Next[++edgecnt]=Head[u];Head[u]=edgecnt;V[edgecnt]=v;W[edgecnt]=w;
     Next[++edgecnt]=Head[v];Head[v]=edgecnt;V[edgecnt]=u;W[edgecnt]=w;
     return;
---
+++ Practice/BZOJ1190.cpp
@@ -0,0 +1,47 @@
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+#define mem(Arr,x) memset(Arr,x,sizeof(Arr))
+#define mp make_pair
+#define ft first
+#define sd second
+#define pb push_back
+
+const int maxN=110;
+const int maxM=1010;
+const int maxL=31;
+
+int n,W;
+int F[maxL][maxM];
+vector<pair<int,int> > Gd[maxL];
+int Sum[maxL];
+
+int main(){
+    while (scanf("%d%d",&n,&W)!=EOF){
+	if (n==-1&&W==-1) break;
+	mem(F,0);mem(Sum,0);
+	for (int i=0;i<maxL;i++) Gd[i].clear();
+
+	int mxl=0;
+	for (int i=1;i<=n;i++){
+	    int a,b,cnt=0;scanf("%d%d",&a,&b);
+	    while (!(a&1)) ++cnt,a>>=1;
+	    mxl=max(mxl,cnt);
+	    Gd[cnt].pb(mp(a,b));Sum[cnt]+=a;
+	}
+
+	for (int i=0;i<=mxl;i++)
+	    for (int j=0,sz=Gd[i].size();j<sz;j++)
+		for (int k=Sum[i];k>=Gd[i][j].ft;k--) F[i][k]=max(F[i][k],F[i][k-Gd[i][j].ft]+Gd[i][j].sd);
+
+	for (int i=1;i<=mxl;i++){
+	    Sum[i]=Sum[i]+(Sum[i-1]+1)>>1;
+	    for (int j=0;j<=Sum[i-1];j++)
+		for (int k=Sum[i];k>=j;k--) F[i][k]=max(F[i][k],
+	}
+    }
+}
