246a9faf1e709d8a63b2403f709cabafc953b0f8
SYCstudio <1726016246@qq.com>
2019-02-02 17:47:24 +0800

    2019.2.2 pm

--- Practice/2019.2.2/BZOJ1190.cpp
+++ Practice/2019.2.2/BZOJ1190.cpp
@@ -3,6 +3,7 @@
 #include<cstring>
 #include<algorithm>
 #include<vector>
+#include<iostream>
 using namespace std;
 
 #define mem(Arr,x) memset(Arr,x,sizeof(Arr))
@@ -13,7 +14,7 @@ using namespace std;
 
 const int maxN=110;
 const int maxM=1010;
-const int maxL=31;
+const int maxL=32;
 
 int n,W;
 int F[maxL][maxM];
@@ -30,6 +31,7 @@ int main(){
 	for (int i=1;i<=n;i++){
 	    int a,b,cnt=0;scanf("%d%d",&a,&b);
 	    while (!(a&1)) ++cnt,a>>=1;
+	    //cout<<a<<" "<<cnt<<" "<<b<<endl;
 	    mxl=max(mxl,cnt);
 	    Gd[cnt].pb(mp(a,b));Sum[cnt]+=a;
 	}
@@ -38,10 +40,15 @@ int main(){
 	    for (int j=0,sz=Gd[i].size();j<sz;j++)
 		for (int k=Sum[i];k>=Gd[i][j].ft;k--) F[i][k]=max(F[i][k],F[i][k-Gd[i][j].ft]+Gd[i][j].sd);
 
+	while ((1<<(mxl+1))<=W) ++mxl;
 	for (int i=1;i<=mxl;i++){
-	    Sum[i]=Sum[i]+(Sum[i-1]+1)>>1;
-	    for (int j=0;j<=Sum[i-1];j++)
-		for (int k=Sum[i];k>=j;k--) F[i][k]=max(F[i][k],
+	    Sum[i]=Sum[i]+((Sum[i-1]+1)>>1);
+	    for (int j=Sum[i];j>=0;j--)
+		for (int k=0;k<=j;k++) F[i][j]=max(F[i][j],F[i][j-k]+F[i-1][min(k*2+((W>>(i-1))&1),Sum[i-1])]);
 	}
+
+	printf("%d\n",F[mxl][1]);
+	//printf("%d\n",Ans);
     }
+    return 0;
 }
---
+++ Practice/2019.2.2/BZOJ5312.cpp
@@ -0,0 +1,95 @@
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+
+#define ls (x<<1)
+#define rs (ls|1)
+
+const int maxN=202000;
+
+class SegmentData{
+public:
+    int ro,ad,mx,dt;
+};
+
+int n,m;
+SegmentData S[maxN<<2];
+
+void Plus(int x,int k);
+void PushDown(int x);
+void Update(int x);
+void Build(int x,int l,int r);
+void Mor(int x,int l,int r,int ql,int qr,int k);
+void Mand(int x,int l,int r,int ql,int qr,int k);
+int Qmx(int x,int l,int r,int ql,int qr);
+
+int main(){
+    scanf("%d%d",&n,&m);
+    Build(1,1,n);
+    while (m--){
+	int opt,l,r,x;scanf("%d%d%d",&opt,&l,&r);
+	if (opt!=3) scanf("%d",&x);
+	if (opt==1) Mand(1,1,n,l,r,x);
+	if (opt==2) Mor(1,1,n,l,r,x);
+	if (opt==3) printf("%d\n",Qmx(1,1,n,l,r));
+    }
+    return 0;
+}
+void Plus(int x,int k){
+    S[x].ro+=k;S[x].ad+=k;S[x].mx+=k;S[x].dt+=k;return;
+}
+void PushDown(int x){
+    if (S[x].dt){
+	Plus(ls,S[x].dt);Plus(rs,S[x].dt);
+	S[x].dt=0;
+    }
+    return;
+}
+void Update(int x){
+    S[x].ro=S[ls].ro|S[rs].ro;
+    S[x].ad=S[ls].ad&S[rs].ad;
+    S[x].mx=max(S[ls].mx,S[rs].mx);
+    return;
+}
+void Build(int x,int l,int r){
+    if (l==r){
+	scanf("%d",&S[x].mx);S[x].ro=S[x].ad=S[x].mx;
+	return;
+    }
+    int mid=(l+r)>>1;
+    Build(ls,l,mid);Build(rs,mid+1,r);
+    Update(x);return;
+}
+void Mor(int x,int l,int r,int ql,int qr,int k){
+    if (l==ql&&r==qr)
+	if ((S[x].ad|k)-S[x].ad==(S[x].ro|k)-S[x].ro){
+	    Plus(x,(S[x].ad|k)-S[x].ad);
+	    return;
+	}
+    int mid=(l+r)>>1;PushDown(x);
+    if (qr<=mid) Mor(ls,l,mid,ql,qr,k);
+    else if (ql>=mid+1) Mor(rs,mid+1,r,ql,qr,k);
+    else Mor(ls,l,mid,ql,mid,k),Mor(rs,mid+1,r,mid+1,qr,k);
+    Update(x);return;
+}
+void Mand(int x,int l,int r,int ql,int qr,int k){
+    if (l==ql&&r==qr)
+	if ((S[x].ad&k)-S[x].ad==(S[x].ro&k)-S[x].ro){
+	    Plus(x,(S[x].ad&k)-S[x].ad);
+	    return;
+	}
+    int mid=(l+r)>>1;PushDown(x);
+    if (qr<=mid) Mand(ls,l,mid,ql,qr,k);
+    else if (ql>=mid+1) Mand(rs,mid+1,r,ql,qr,k);
+    else Mand(ls,l,mid,ql,mid,k),Mand(rs,mid+1,r,mid+1,qr,k);
+    Update(x);return;
+}
+int Qmx(int x,int l,int r,int ql,int qr){
+    if (l==ql&&r==qr) return S[x].mx;
+    int mid=(l+r)>>1;PushDown(x);
+    if (qr<=mid) Qmx(ls,l,mid,ql,qr);
+    else if (ql>=mid+1) Qmx(rs,mid+1,r,ql,qr);
+    else return max(Qmx(ls,l,mid,ql,mid),Qmx(rs,mid+1,r,mid+1,qr));
+}
