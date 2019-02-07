64ba51e42a8d5893ae799bd08b9dcc4fe5e69bbd
SYCstudio <1726016246@qq.com>
2019-02-08 00:46:30 +0800

    2019.2.7 night

--- Practice/BZOJ4909.cpp
+++ Practice/BZOJ4909.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 #define mem(Arr,x) memset(Arr,x,sizeof(Arr))
 
-double X,Y;
+int X,Y;
 
 namespace brute{
     const int maxN=40400;
@@ -17,10 +17,10 @@ namespace brute{
 }
 
 int main(){
-    freopen("in","r",stdin);freopen("out","w",stdout);
+    //freopen("in","r",stdin);freopen("out","w",stdout);
     int Case;scanf("%d",&Case);
     while (Case--){
-        scanf("%lf%lf",&X,&Y);
+        scanf("%d%d",&X,&Y);
         if (Y<=1600) brute::main();
     }
     return 0;
@@ -28,17 +28,25 @@ int main(){
 
 namespace brute{
     int main(){
-        cerr<<X<<" "<<Y<<endl;
-        mem(F,0);X--;
-        int now=1;F[now][0]=1;
+        mem(F,0);
+        int now=1;for (int i=0;i<=(X-1)*Y;i++) F[now][i]=1;
         for (int i=1;i<=Y;i++){
-            now^=1;mem(F[now],sizeof(int)*(i*X));
-            for (int j=0;j<=(i-1)*X;j++)
-                for (int k=0;k<=X;k++)
-                    F[now][j+k]+=F[now^1][j]/(X+1);
-            //for (int j=0;j<=i*X;j++) cout<<F[now][j]<<" ";cout<<endl;
+            now^=1;mem(F[now],0);
+            for (int j=0;j<=i*(X-1);j++){
+                /*
+                for (int k=0;k<X;k++)
+                    F[now][j+k]+=F[now^1][j]/X;
+                //*/
+
+                double f=F[now^1][min(j,(i-1)*(X-1))];
+                if (j>=(int)X) f-=F[now^1][min(j-X,(i-1)*(X-1))];
+                f=f/X;
+                F[now][j]=f+((j>0)?(F[now][j-1]):0);
+                //*/
+            }
+            //for (int j=0;j<=i*(X-1);j++) cout<<F[now][j]<<" ";cout<<endl;
         }
-        for (int i=1;i<=Y*X;i++) F[now][i]+=F[now][i-1];
+        //for (int i=1;i<=(X-1)*Y;i++) F[now][i]+=F[now][i-1];
         for (int ti=1;ti<=10;ti++){
             int A,B;scanf("%d%d",&A,&B);
             printf("%.12lf\n",F[now][B]-((A>=1)?F[now][A-1]:0));
