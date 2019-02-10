c08ce346c2ef1c88970ed52cf3437551ee9e2bd8
SYCstudio <1726016246@qq.com>
2019-02-10 11:59:18 +0800

    2019.2.10 am

--- Practice/2019.2.10/ATYahoo2019D.cpp
+++ Practice/2019.2.10/ATYahoo2019D.cpp
@@ -11,7 +11,8 @@ ll Sum[maxN],F[maxN],G[maxN],Even[maxN],Cost[maxN];
 int main(){
     scanf("%d",&n);
     for (int i=1;i<=n;i++){
-        int x;scanf("%d",&x);Sum[i]=Sum[i-1]+x;Even[i]=Even[i-1]+(~x&1);Cost[i]=Cost[i-1]+((x<=2)?(2-x):(x&1));
+        ll x;scanf("%lld",&x);Sum[i]=Sum[i-1]+x;
+        Even[i]=Even[i-1]+(~x&1);Cost[i]=Cost[i-1]+(x<=2?2-x:(x&1));
     }
     //for (int i=1;i<=n;i++) cout<<Sum[i]<<" ";cout<<endl;
     //for (int i=1;i<=n;i++) cout<<Even[i]<<" ";cout<<endl;
@@ -19,10 +20,10 @@ int main(){
     ll mn=0;
     for (int i=1;i<=n;i++) mn=min(mn,-Cost[i]+Sum[i]),F[i]=mn+Cost[i];
     mn=Cost[n];
-    for (int i=n-1;i>=1;i--) mn=min(mn,Sum[n]+Cost[i]-Sum[i]),G[i]=mn-Cost[i];
+    for (int i=n-1;i>=0;i--) mn=min(mn,Sum[n]+Cost[i]-Sum[i]),G[i]=mn-Cost[i];
 
-    //for (int i=1;i<=n;i++) cout<<F[i]<<" ";cout<<endl;
-    //for (int i=1;i<=n;i++) cout<<G[i]<<" ";cout<<endl;
+    //for (int i=0;i<=n;i++) cout<<F[i]<<" ";cout<<endl;
+    //for (int i=0;i<=n;i++) cout<<G[i]<<" ";cout<<endl;
 
     ll Ans=INF;mn=INF;
     for (int i=0;i<=n;i++) mn=min(mn,F[i]-Even[i]),Ans=min(Ans,mn+Even[i]+G[i]);
