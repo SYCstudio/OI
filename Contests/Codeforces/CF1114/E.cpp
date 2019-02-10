5d78c47f77c4f4b532a4b09768bf37f4b51c6cf7
SYCstudio <1726016246@qq.com>
2019-02-11 01:45:26 +0800

    2019.2.10 CF

--- Contests/Codeforces/CF1114/E.cpp
+++ Contests/Codeforces/CF1114/E.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 
 int main(){
-    srand(20020729);
-    int n;scanf("%d",&n);int cnt=40;
-    int l=0,r=1e9,mx;
+    srand(20020622);
+    int n;scanf("%d",&n);int cnt=60;
+    int l=0,r=1e9,mx=0;
     while (l<=r){
         int mid=(l+r)>>1;
-        cout<<"> "<<mid<<endl<<flush;
+        cout<<"> "<<mid<<endl;fflush(stdout);
         int b;cin>>b;--cnt;
         if (b) mx=mid+1,l=mid+1;
         else r=mid-1;
     }
     int d=0;
-    while (cnt--){
+    int Seq[50];Seq[0]=mx;
+    for (int i=1;i<=cnt;i++){
         int p=rand()%n+1;
-        cout<<"? "<<p<<endl<<flush;
-        int key;cin>>key;
-        if (d==0) d=mx-key;
-        else d=__gcd(d,mx-key);
+        cout<<"? "<<p<<endl;fflush(stdout);
+        cin>>Seq[i];
+        for (int j=0;j<i;j++) d=__gcd(d,abs(Seq[i]-Seq[j]));
     }
-    printf("%d %d\n",mx-(n-1)*d,d);return 0;
+    printf("! %d %d\n",mx-(n-1)*d,d);return 0;
 }
