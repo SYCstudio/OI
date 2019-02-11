65d8fabe3954cd32d2c251e19a012bfa1e500a96
SYCstudio <1726016246@qq.com>
2019-02-11 11:52:57 +0800

    2019.2.11 am

---
+++ Practice/2019.2.11/CF1114D.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define mem(Arr,x) memset(Arr,x,sizeof(Arr))
+const int maxN=5010;
+int n,Col[maxN];
+int F[maxN][maxN][2];
+
+int c(int l,int r){return Col[l]!=Col[r];}
+int main(){
+    scanf("%d",&n);for (int i=1;i<=n;i++) scanf("%d",&Col[i]);
+    mem(F,63);
+    for (int i=1;i<=n;i++) F[i][i][0]=F[i][i][1]=0;
+    for (int len=1;len<n;len++)
+        for (int l=1;l+len-1<=n;l++)
+            for (int b=0;b<=1;b++){
+                int r=l+len-1,p=(b?r:l);
+                if (l>1) F[l-1][r][0]=min(F[l-1][r][0],F[l][r][b]+c(l-1,p));
+                if (r<n) F[l][r+1][1]=min(F[l][r+1][1],F[l][r][b]+c(r+1,p));
+            }
+    printf("%d\n",min(F[1][n][0],F[1][n][1]));return 0;
+}
--- Practice/2019.2.11/CF1114E.cpp
+++ Practice/2019.2.11/CF1114E.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int make(int l,int r);
+
 int main(){
     srand(20020622);
     int n;scanf("%d",&n);int cnt=60;
@@ -9,16 +11,20 @@ int main(){
         int mid=(l+r)>>1;
         cout<<"> "<<mid<<endl;fflush(stdout);
         int b;cin>>b;--cnt;
-        if (b) mx=mid+1,l=mid+1;
-        else r=mid-1;
+        if (b) l=mid+1;
+        else mx=mid,r=mid-1;
     }
     int d=0;
     int Seq[50];Seq[0]=mx;
     for (int i=1;i<=cnt;i++){
-        int p=rand()%n+1;
+        int p=make(1,n);
         cout<<"? "<<p<<endl;fflush(stdout);
         cin>>Seq[i];
         for (int j=0;j<i;j++) d=__gcd(d,abs(Seq[i]-Seq[j]));
     }
     printf("! %d %d\n",mx-(n-1)*d,d);return 0;
+}
+int make(int l,int r){
+    double dou=1.0*rand()/RAND_MAX;
+    return min(r,(int)(dou*(r-l+1))+l);
 }
