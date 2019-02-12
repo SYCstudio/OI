b6e3b919f12c6fa9f0d728dccbd2a93d879ee9f3
SYCstudio <1726016246@qq.com>
2019-02-12 23:03:24 +0800

    2019.2.12 night

---
+++ Practice/LOJ2392.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+
+const int maxN=101000;
+
+int n,K,T;
+int X[maxN];
+
+int main(){
+    scanf("%d",&n);
+}
