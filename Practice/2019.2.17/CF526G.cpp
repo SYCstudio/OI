52789720cf4f23c62353c0b62dfda40b17f10ed4
SYCstudio <1726016246@qq.com>
2019-02-17 23:02:58 +0800

    2019.2.17 night

--- Practice/2019.2.17/CF526G.cpp
+++ Practice/2019.2.17/CF526G.cpp
@@ -5,16 +5,17 @@
 using namespace std;
 
 #define mem(Arr,x) memset(Arr,x,sizeof(Arr))
+#define pw(x) (1<<(x))
 const int maxN=100100;
 const int maxM=maxN<<1;
 const int maxB=17;
 
-int n,Q;
+int n,Q,Log[maxN];
 int edgecnt=-1,Head[maxN],Next[maxM],V[maxM],W[maxM],Dst[maxN];
 
 class Tree{
     public:
-    int root,Dep[maxN],Fa[maxN],Dst[maxN],Mx[maxN],ST[maxB][maxN];
+    int root,Dep[maxN],Fa[maxN],Dst[maxN],Mx[maxN],ST[maxB][maxN],Mxd[maxB][maxN],dfncnt,dfn[maxN],lst[maxN];
     int lcnt,Mark[maxN];
     pair<int,int> Leaf[maxN];
     void dfs_init(int u,int fa){
@@ -27,13 +28,14 @@ class Tree{
         return;
     }
     void dfs_push(int u,int fa,int w){
-        Dep[u]=Dep[fa]+1;
+        Dep[u]=Dep[fa]+1;dfn[u]=++dfncnt;Mxd[0][dfncnt]=Dst[u];
         if (Mx[u]==0) Leaf[++lcnt]=make_pair(w,u);
         else{
             Fa[Mx[u]]=u;
             for (int i=Head[u];i!=-1;i=Next[i])
-                if (V[i]!=fa) Dst[V[i]]=Dst[u]+W[i],dfs_push(V[i],u,(V[i]==Mx[u])?Dst[u]+W[i]:W[i]);
+                if (V[i]!=fa) Dst[V[i]]=Dst[u]+W[i],dfs_push(V[i],u,(V[i]==Mx[u])?w+W[i]:W[i]);
         }
+        lst[u]=dfncnt;
         return;
     }
     void Init(int r){
@@ -44,27 +46,29 @@ class Tree{
             int now=Leaf[i].second;
             while (now) Mark[now]=i,now=Fa[now];
         }
-        cout<<"root:"<<root<<endl;
-        for (int i=1;i<=n;i++) cout<<ST[0][i]<<" ";cout<<endl;
-        for (int i=1;i<=n;i++) cout<<Mark[i]<<" ";cout<<endl;
-        for (int i=1;i<=n;i++) cout<<Dst[i]<<" ";cout<<endl;
-        for (int i=1;i<=n;i++) cout<<Dep[i]<<" ";cout<<endl;
-        for (int i=1;i<=lcnt;i++) cout<<Leaf[i].first<<" "<<Leaf[i].second<<endl;
+        for (int i=1;i<maxB;i++) for (int j=1;j+pw(i)-1<=n;j++) Mxd[i][j]=max(Mxd[i-1][j],Mxd[i-1][j+pw(i-1)]);
+        //cout<<"init on:"<<root<<endl;
+        //for (int i=1;i<=lcnt;i++) cout<<"("<<Leaf[i].first-Leaf[i-1].first<<","<<Leaf[i].second<<")"<<endl;
         return;
     }
     int LCA(int u,int v){
         if (Dep[u]<Dep[v]) swap(u,v);for (int i=maxB-1;i>=0;i--) if (ST[i][u]&&Dep[ST[i][u]]>=Dep[v]) u=ST[i][u];
-        if (u==v) return u;for (int i=maxB-1;i>=0;i--) if (ST[i][u]&&ST[i][v]&&ST[i][u]!=ST[i][v]) u=ST[i][u],v=ST[i][v],cout<<"skip:"<<u<<" "<<v<<endl;
-        cout<<"final:"<<u<<" "<<v<<" "<<ST[0][u]<<endl;
+        if (u==v) return u;for (int i=maxB-1;i>=0;i--) if (ST[i][u]&&ST[i][v]&&ST[i][u]!=ST[i][v]) u=ST[i][u],v=ST[i][v];
         return ST[0][u];
     }
     int GET(int u,int cnt){
         cnt=min(cnt*2-1,lcnt);
         if (Mark[u]<=cnt) return Leaf[cnt].first;
-        int lca=LCA(u,Leaf[cnt].second);
-        cout<<"GET:"<<u<<" "<<cnt<<endl;
-        cout<<"lca:"<<u<<" "<<Leaf[cnt].second<<"|"<<lca<<endl;
-        return Leaf[cnt].first-(Dst[Leaf[cnt].second]-Dst[lca])+(Dst[u]-Dst[lca]);
+        int ret=Leaf[cnt-1].first;
+        int now=u;for (int i=maxB-1;i>=0;i--) if (ST[i][now]&&Mark[ST[i][now]]>cnt) now=ST[i][now];now=ST[0][now];
+        //cout<<"root:"<<root<<" "<<u<<" "<<cnt<<endl;
+        //cout<<now<<" "<<ret<<endl;
+        int l=dfn[u],r=lst[u],lg=Log[r-l+1];
+        int mxd=max(Mxd[lg][l],Mxd[lg][r-pw(lg)+1]);
+        //cout<<"mxd:"<<mxd<<endl;
+        ret=ret+mxd-Dst[now];
+        //cout<<"RET:"<<ret<<" "<<Leaf[cnt].first+Dst[u]-Dst[Leaf[Mark[now]].second]<<endl;
+        return max(ret,Leaf[cnt].first+mxd-Dst[Leaf[Mark[now]].second]);
     }
 };
 
@@ -73,7 +77,8 @@ Tree T1,T2;
 void Add_Edge(int u,int v,int w);
 void Bfs(int S);
 int main(){
-    freopen("in","r",stdin);freopen("out","w",stdout);
+    for (int i=2;i<maxN;i++) Log[i]=Log[i>>1]+1;
+    //freopen("in","r",stdin);freopen("out","w",stdout);
     scanf("%d%d",&n,&Q);mem(Head,-1);
     for (int i=1;i<n;i++){
         int u,v,w;scanf("%d%d%d",&u,&v,&w);
@@ -82,9 +87,10 @@ int main(){
     int S,T;
     Bfs(1);S=1;for (int i=1;i<=n;i++) if (Dst[S]<Dst[i]) S=i;
     Bfs(S);T=1;for (int i=1;i<=n;i++) if (Dst[T]<Dst[i]) T=i;
-    cout<<S<<" "<<T<<endl;
+    //cout<<S<<" "<<T<<endl;
     T1.Init(S);T2.Init(T);int lstans=0;
     while (Q--){
+        //cout<<"running on :"<<Q<<endl;
         int x,y;scanf("%d%d",&x,&y);
         x=(x+lstans-1)%n+1;y=(y+lstans-1)%n+1;
         printf("%d\n",lstans=max(T1.GET(x,y),T2.GET(x,y)));
