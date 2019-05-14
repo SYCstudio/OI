54d53b213c1f4153dbfb2a059f9a56ba09d6bda8
SYCstudio <1726016246@qq.com>
2019-05-15 00:17:38 +0800

    update

--- Practice/CF628D.cpp
+++ Practice/CF628D.cpp
@@ -22,13 +22,14 @@ int main(){
     ++R[1];for (int i=1;i<=l2;i++) R[i+1]+=R[i]/10,R[i]%=10;
     if (R[l2+1]) ++l2;
 
-    printf("%d\n",(Calc(l2,R)-Calc(l1,L)+1)%Mod);
+    printf("%d\n",(Calc(l2,R)-Calc(l1,L)+Mod)%Mod);
     return 0;
 }
 void Plus(int &x,int y){
     x+=y;if (x>=Mod) x-=Mod;return;
 }
 int Calc(int len,int *Num){
+	for (int i=1;i<=len;i++) cout<<Num[i]<<" ";cout<<endl;
     memset(F,0,sizeof(F));
     F[0][1][0]=1;
     for (int i=1,mul=1;i<=len;i++,mul=10ll*mul%M)
@@ -36,15 +37,19 @@ int Calc(int len,int *Num){
 	    for (int r=0;r<M;r++)
 		if (F[i-1][b][r]){
 		    int key=F[i-1][b][r];
-		    if (~i&1){
-			for (int k=D;k<=D;k++)
-			    if ((b==0&&D<=Num[i])||(b==1&&D<Num[i])) Plus(F[i][0][(r+1ll*k*mul%M)%M],key);
-			    else Plus(F[i][1][(r+1ll*k*mul%M)%M],key);
+		    if ((len-i+1)%2==0){
+				for (int k=D;k<=D;k++){
+					cout<<"("<<i-1<<" "<<b<<" "<<r<<") -> ("<<i<<" "<<(!((b==0&&k<=Num[i])||(b==1&&k<Num[i])))<<" "<<(r+1ll*k*mul%M)%M<<")"<<endl;
+					if ((b==0&&k<=Num[i])||(b==1&&k<Num[i])) Plus(F[i][0][(r+1ll*k*mul%M)%M],key);
+					else Plus(F[i][1][(r+1ll*k*mul%M)%M],key);
+				}
 		    }
 		    else
-			for (int k=0;k<=9;k++)
-			    if ((b==0&&D<=Num[i])||(b==1&&D<Num[i])) Plus(F[i][0][(r+1ll*k*mul%M)%M],key);
-			    else Plus(F[i][1][(r+1ll*k*mul%M)%M],key);
+				for (int k=0;k<=9;k++){
+					cout<<"("<<i-1<<" "<<b<<" "<<r<<") -> ("<<i<<" "<<(!((b==0&&k<=Num[i])||(b==1&&k<Num[i])))<<" "<<(r+1ll*k*mul%M)%M<<")"<<endl;
+					if ((b==0&&k<=Num[i])||(b==1&&k<Num[i])) Plus(F[i][0][(r+1ll*k*mul%M)%M],key);
+					else Plus(F[i][1][(r+1ll*k*mul%M)%M],key);
+				}
 		}
     cout<<F[len][0][0]<<endl;
     return F[len][0][0];
