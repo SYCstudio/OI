3474efb556ad1a5936c38959a27f734a4457d69e
SYCstudio <1726016246@qq.com>
2019-06-15 11:53:25 +0800

    fix comet46E, practice BJOI2019

---
+++ Practice/2019.6.15/LOJ3091.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+const int maxN=110;
+const int maxM=20200;
+
+int n,m,S;
+vector<int> Ser[maxN];
+int F[maxN][maxM],Num[maxN];
+
+int main(){
+    scanf("%d%d%d",&S,&n,&m);
+    for (int i=1;i<=S;i++)
+	for (int j=1;j<=n;j++){
+	    int k;scanf("%d",&k);Ser[j].pb(k);
+	}
+    F[0][0]=0;
+    for (int i=1;i<=n;i++){
+	for (int j=0;j<=m;j++) F[i][j]=F[i-1][j];
+	int num=0;for (int j=0;j<Ser[i].size();j++) Num[++num]=Ser[i][j]*2+1;
+	sort(&Num[1],&Num[num+1]);
+	for (int j=1;j<=num;j++)
+	    for (int l=Num[j];l<=m;l++) F[i][l]=max(F[i][l],F[i-1][l-Num[j]]+j*i);
+	//for (int j=0;j<=m;j++) cout<<F[i][j]<<" ";cout<<endl;
+    }
+    int Ans=0;for (int i=0;i<=m;i++) Ans=max(Ans,F[n][i]);
+    printf("%d\n",Ans);return 0;
+}
---
+++ Practice/2019.6.15/comet46E.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define Inv(x) QPow(x,Mod-2)
+const int maxN=804000;
+const int Mod=998244353;
+const int G=3;
+
+int n,P,Q;
+int Fc[maxN],Ifc[maxN],Ra[maxN];
+int A[maxN],B[maxN],C[maxN],D[maxN];
+
+int QPow(int x,int cnt);
+void NTT(int *P,int N,int opt);
+int main(){
+    Fc[0]=Ifc[0]=1;for (int i=1;i<maxN;i++) Fc[i]=1ll*Fc[i-1]*i%Mod;
+    Ifc[maxN-1]=Inv(Fc[maxN-1]);for (int i=maxN-2;i>=1;i--) Ifc[i]=1ll*Ifc[i+1]*(i+1)%Mod;
+
+    int a,b;scanf("%d%d%d",&n,&a,&b);P=1ll*a*Inv(b)%Mod;Q=(Mod+1-P)%Mod;
+    int N=1;while (N<=n+n) N<<=1;
+    for (int i=0;i<=n;i++){
+	A[i]=1ll*Fc[i]*(1-QPow(Q,i+1)+Mod)%Mod;
+	A[i]=Inv(A[i]);
+	if (i&1) A[i]=(Mod-A[i]+Mod)%Mod;
+	if (i) B[i]=Ifc[i-1];
+    }
+    NTT(A,N,1);NTT(B,N,1);
+    for (int i=0;i<N;i++) A[i]=1ll*A[i]*B[i]%Mod;
+    NTT(A,N,-1);
+    for (int i=0;i<=n;i++) A[i]=1ll*A[i]*P%Mod*Fc[i-1]%Mod;
+
+    for (int i=0;i<=n;i++){
+	C[i]=1ll*A[n-i]*QPow(P,i)%Mod*Ifc[i]%Mod;
+	if (i) D[i]=1ll*QPow(Q,i-1)*Ifc[i-1]%Mod;
+    }
+    NTT(C,N,1);NTT(D,N,1);
+    for (int i=0;i<N;i++) C[i]=1ll*C[i]*D[i]%Mod;
+    NTT(C,N,-1);
+    for (int i=1;i<=n;i++) C[i]=1ll*C[i]*Fc[i-1]%Mod;
+    
+    for (int i=1;i<=n;i++) printf("%d\n",C[i]);
+    return 0;
+}
+int QPow(int x,int cnt){
+    int ret=1;
+    while (cnt){
+	if (cnt&1) ret=1ll*ret*x%Mod;
+	cnt>>=1;x=1ll*x*x%Mod;
+    }
+    return ret;
+}
+void NTT(int *P,int N,int opt){
+    int _N=1,l=-1;while (_N<N) _N<<=1,++l;
+    for (int i=0;i<N;i++) Ra[i]=(Ra[i>>1]>>1)|((i&1)<<l);
+    for (int i=0;i<N;i++) if (i<Ra[i]) swap(P[i],P[Ra[i]]);
+    for (int i=1;i<N;i<<=1){
+	int dw=QPow(G,(Mod-1)/(i<<1));
+	if (opt==-1) dw=Inv(dw);
+	for (int j=0;j<N;j+=(i<<1))
+	    for (int k=0,w=1;k<i;k++,w=1ll*w*dw%Mod){
+		int X=P[j+k],Y=1ll*P[j+k+i]*w%Mod;
+		P[j+k]=(X+Y)%Mod;P[j+k+i]=(X-Y+Mod)%Mod;
+	    }
+    }
+    if (opt==-1){
+	int inv=Inv(N);for (int i=0;i<N;i++) P[i]=1ll*P[i]*inv%Mod;
+    }
+    return;
+}
