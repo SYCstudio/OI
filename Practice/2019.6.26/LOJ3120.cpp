adc98aa70f7b2bd5e3e7e0d486d9b13a4be19e2e
SYCstudio <1726016246@qq.com>
2019-06-26 11:58:56 +0800

    CTS2019

---
+++ Practice/2019.6.26/LOJ3120.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define Inv(x) QPow(x,Mod-2)
+const int maxN=101000*8;
+const int Mod=998244353;
+const int g=3;
+
+int Fc[maxN],Ifc[maxN],Ra[maxN];
+int n,m,D;
+int F[maxN],G[maxN];
+
+int QPow(int x,int cnt);
+int Binom(int n,int m);
+void NTT(int *P,int N,int opt);
+int main(){
+    Fc[0]=Ifc[0]=1;for (int i=1;i<maxN;i++) Fc[i]=1ll*Fc[i-1]*i%Mod;
+    Ifc[maxN-1]=Inv(Fc[maxN-1]);for (int i=maxN-2;i>=1;i--) Ifc[i]=1ll*Ifc[i+1]*(i+1)%Mod;
+    
+    scanf("%d%d%d",&D,&n,&m);
+    if (n<m+m){
+	puts("0");return 0;
+    }
+    else if (D<=n-m-m){
+	printf("%d\n",QPow(D,n));return 0;
+    }
+
+    for (int i=0;i<=D;i++){
+	F[i]=1ll*QPow((D-i-i+Mod)%Mod,n)*Ifc[i]%Mod;
+	G[i]=Ifc[i];
+	if (i&1) F[i]=(Mod-F[i])%Mod;
+    }
+
+    int N=1;while (N<=D+D) N<<=1;
+    NTT(F,N,1);NTT(G,N,1);
+    for (int i=0;i<N;i++) F[i]=1ll*F[i]*G[i]%Mod;
+    NTT(F,N,-1);memset(G,0,sizeof(G));
+    for (int i=0;i<=D;i++) F[i]=1ll*F[i]*Inv(1ll*QPow(2,i)*Fc[D-i]%Mod*Ifc[D]%Mod)%Mod;
+
+    for (int i=D+1;i<N;i++) F[i]=0;
+    for (int i=0;i<=D;i++){
+	F[i]=1ll*F[i]*Fc[i]%Mod;
+	G[i]=Ifc[i];
+	if (i&1) G[i]=(Mod-G[i])%Mod;
+    }
+    reverse(&G[0],&G[D+1]);
+    NTT(F,N,1);NTT(G,N,1);
+    for (int i=0;i<N;i++) F[i]=1ll*F[i]*G[i]%Mod;
+    NTT(F,N,-1);
+    for (int i=0;i<=D;i++) F[i]=1ll*F[D+i]*Ifc[i]%Mod;
+
+    int Ans=0;
+    for (int i=0;i<=n-m-m;i++) Ans=(Ans+F[i])%Mod;
+    printf("%d\n",Ans);return 0;
+}
+int QPow(int x,int cnt){
+    int ret=1;
+    while (cnt){
+	if (cnt&1) ret=1ll*ret*x%Mod;
+	x=1ll*x*x%Mod;cnt>>=1;
+    }
+    return ret;
+}
+int Binom(int n,int m){
+    if (n<0||m<0||n<m) return 0;
+    return 1ll*Fc[n]*Ifc[m]%Mod*Ifc[n-m]%Mod;
+}
+void NTT(int *P,int N,int opt){
+    int _N=1,l=-1;while (_N<N) ++l,_N<<=1;
+    for (int i=0;i<N;i++) Ra[i]=(Ra[i>>1]>>1)|((i&1)<<l);
+    for (int i=0;i<N;i++) if (i<Ra[i]) swap(P[i],P[Ra[i]]);
+    for (int i=1;i<N;i<<=1){
+	int dw=QPow(g,(Mod-1)/(i<<1));
+	if (opt==-1) dw=Inv(dw);
+	for (int j=0;j<N;j+=(i<<1))
+	    for (int k=0,w=1;k<i;k++,w=1ll*w*dw%Mod){
+		int X=P[j+k],Y=1ll*P[j+k+i]*w%Mod;
+		P[j+k]=(X+Y)%Mod;P[j+k+i]=(X-Y+Mod)%Mod;
+	    }
+    }
+    if (opt==-1){
+	int inv=Inv(N);for (int i=0;i<N;i++) P[i]=1ll*P[i]*inv%Mod;
+    }
+    return;
+}
---
+++ Practice/2019.6.26/LOJ6433.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pw(x) (1<<(x))
+
+const int maxN=20;
+const int maxM=pw(maxN)+10;
+const int Mod=998244353;
+
+int n,N,A[maxN];
+int Sm[maxM],F[maxM],G[maxM],C[maxM];
+
+void Plus(int &x,int y);
+int main(){
+    scanf("%d",&n);N=pw(n);
+    for (int i=0;i<n;i++) scanf("%d",&A[i]),Sm[pw(i)]=A[i];
+    for (int S=1;S<N;S++) Sm[S]=Sm[S-((S)&(-S))]+Sm[(S)&(-S)];
+    
+    for (int i=0;i<n;i++) F[pw(i)]=1;
+    for (int S=1;S<N;S++)
+	if (Sm[S]>0)
+	    for (int i=0;i<n;i++)
+		if ((S&pw(i))==0)
+		Plus(F[S|pw(i)],F[S]);
+    G[0]=1;
+    for (int S=1;S<N;S++)
+	if (Sm[S]<=0)
+	    for (int i=0;i<n;i++)
+		if (S&pw(i))
+		    Plus(G[S],G[S^pw(i)]);
+    //for (int S=0;S<N;S++) cout<<F[S]<<" ";cout<<endl;
+    //for (int S=0;S<N;S++) cout<<G[S]<<" ";cout<<endl;
+    //for (int S=0;S<N;S++) cout<<Sm[S]<<" ";cout<<endl;
+    int Ans=0;
+    for (int S=0;S<N;S++){
+	//cout<<1ll*Sm[S]*F[S]%Mod*G[(N-1)^S]%Mod<<" ";
+	Ans=(Ans+1ll*Sm[S]*F[S]%Mod*G[(N-1)^S]%Mod)%Mod;
+	Ans=(Ans+Mod)%Mod;
+    }//cout<<endl;
+    printf("%d\n",Ans);return 0;
+}
+void Plus(int &x,int y){
+    x+=y;if (x>=Mod) x-=Mod;return;
+}
