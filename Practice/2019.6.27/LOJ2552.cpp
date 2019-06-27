c7a286cdb490c6b6aa0a957caf879f2a9c992494
SYCstudio <1726016246@qq.com>
2019-06-27 23:01:16 +0800

    CTSC2018

--- Practice/2019.6.27/LOJ2552.cpp
+++ Practice/2019.6.27/LOJ2552.cpp
@@ -9,12 +9,12 @@ const int inf=2000000000;
 
 int n,Q;
 int Mlim[maxN],Fm[maxN][maxM],Gm[maxM];
-int dppre[maxN][maxN];
-int dpsuf[maxN][maxN];
+int dp[maxN][maxN],dpg[maxN];
+int Iv[maxN];
 
 int QPow(int x,int cnt);
 int main(){
-    freopen("4.in","r",stdin);freopen("out","w",stdout);
+    for (int i=1;i<maxN;i++) Iv[i]=Inv(i);
     scanf("%d",&n);
     for (int i=1;i<=n;i++){
 	scanf("%d",&Mlim[i]);Fm[i][Mlim[i]]=1;
@@ -37,38 +37,44 @@ int main(){
 	    P[0]=-inf;
 	    for (int i=1;i<=K;i++) scanf("%d",&P[i]);
 	    
-	    dppre[0][0]=1;
-	    for (int i=1;i<=K;i++){
-		for (int j=0;j<=i;j++) dppre[i][j]=1ll*dppre[i-1][j]*Fm[P[i]][0]%Mod;
-		for (int j=1;j<=i;j++) dppre[i][j]=(dppre[i][j]+1ll*(1-Fm[P[i]][0]+Mod)%Mod*dppre[i-1][j-1]%Mod)%Mod;
-	    }
-	    dpsuf[K+1][0]=1;
-	    for (int i=K;i>=1;i--){
-		for (int j=0;j<=K-i+1;j++) dpsuf[i][j]=1ll*dpsuf[i+1][j]*Fm[P[i]][0]%Mod;
-		for (int j=1;j<=K-i+1;j++) dpsuf[i][j]=(dpsuf[i][j]+1ll*(1-Fm[P[i]][0]+Mod)%Mod*dpsuf[i+1][j-1]%Mod)%Mod;
-	    }
+	    dp[0][0]=1;int survive=0;
+	    for (int i=1;i<=K;i++)
+		if (Fm[P[i]][0]==0){
+		    ++survive;
+		    for (int j=0;j<=i;j++) dp[i][j]=dp[i-1][j];
+		}
+		else{
+		    for (int j=0;j<=i;j++) dp[i][j]=1ll*dp[i-1][j]*Fm[P[i]][0]%Mod;
+		    for (int j=1;j<=i;j++) dp[i][j]=(dp[i][j]+1ll*(1-Fm[P[i]][0]+Mod)%Mod*dp[i-1][j-1]%Mod)%Mod;
+		}
 
 	    /*
 	    for (int i=1;i<=K;i++){
-		for (int j=0;j<=K;j++) cout<<dppre[i][j]<<" ";cout<<endl;
-	    }cout<<endl;
-	    for (int i=1;i<=K;i++){
-		for (int j=0;j<=K;j++) cout<<dpsuf[i][j]<<" ";cout<<endl;
+		for (int j=0;j<=K;j++) cout<<dp[i][j]<<" ";cout<<endl;
 	    }cout<<endl;
 	    //*/
 	    
-	    for (int i=1;i<=K;i++){
-		int suv=(1-Fm[P[i]][0]+Mod)%Mod,Ans=0;
-		for (int a=0;a<=K-1;a++){
-		    int sum=0;
-		    for (int b=0;b<=a&&b<i;b++)
-			sum=(sum+1ll*dppre[i-1][b]*dpsuf[i+1][a-b]%Mod)%Mod;
-		    Ans=(Ans+1ll*sum*Inv(a+1)%Mod)%Mod;
+	    for (int i=1;i<=K;i++)
+		if (Fm[P[i]][0]==0){
+		    int Ans=0;
+		    for (int j=0;j<=K;j++) Ans=(Ans+1ll*dp[K][j]*Iv[j+survive]%Mod)%Mod;
+		    printf("%d ",Ans);
 		}
-		Ans=1ll*Ans*suv%Mod;
-		printf("%d ",Ans);
-	    }
-	    for (int i=0;i<=K+1;i++) for (int j=0;j<=K;j++) dppre[i][j]=dpsuf[i][j]=0;
+		else{
+		    int suv=(1-Fm[P[i]][0]+Mod)%Mod,Ans=0;
+		    int inv1=Inv(Fm[P[i]][0]),mul2=(1-Fm[P[i]][0]+Mod)%Mod;
+		    dpg[0]=1ll*dp[K][0]*inv1%Mod;
+		    for (int j=1;j<=K;j++){
+			int dpk=(dp[K][j]-1ll*dpg[j-1]*mul2%Mod+Mod)%Mod;
+			dpg[j]=1ll*dpk*inv1%Mod;
+		    }
+		    //cout<<"Q:"<<Q<<" "<<i<<":";
+		    //for (int j=0;j<K;j++) cout<<dpg[j]<<" ";cout<<endl;
+		    for (int j=0;j<K;j++) Ans=(Ans+1ll*Iv[j+1+survive]*dpg[j]%Mod)%Mod;
+		    Ans=1ll*Ans*suv%Mod;
+		    printf("%d ",Ans);
+		}
+	    for (int i=0;i<=K+1;i++) for (int j=0;j<=K;j++) dp[i][j]=0;
 	    puts("");
 	}
 	/*
