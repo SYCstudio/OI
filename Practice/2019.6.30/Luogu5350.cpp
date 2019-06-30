2b944a4ea688869254b84133bd7d7e2fd0b53533
SYCstudio <1726016246@qq.com>
2019-06-30 17:52:25 +0800

    Pra

---
+++ Practice/2019.6.30/Luogu5350.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int maxN=303000;
+const int Mod=1e9+7;
+
+class TreapData{
+public:
+    int ls,rs,siz,key,sum,plus;
+};
+
+int n;
+int Seq[maxN];
+TreapData T[maxN*20];
+
+int main(){
+    return 0;
+}
