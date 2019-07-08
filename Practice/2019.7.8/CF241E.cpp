7d0b0646ac92f8eaebd26b46fb7e5ce6ce9c7aee
SYCstudio <1726016246@qq.com>
2019-07-08 17:49:20 +0800

    Graph

---
+++ Practice/2019.7.8/CF241E.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define ft first
+#define sd second
+#define mp make_pair
+const int maxN=1100;
+const int maxM=5050;
+
+int n,m;
+int U[maxM],V[maxM];
+vector<int> T1[maxN],T2[maxN];
+int M1[maxN],M2[maxN];
+vector<pair<int,int> > G[maxN];
+int Dst[maxN],inq[maxN],vis[maxN];
+queue<int> Qu;
+
+void dfs1(int u);
+void dfs2(int u);
+int main(){
+    scanf("%d%d",&n,&m);
+    for (int i=1;i<=m;i++){
+	scanf("%d%d",&U[i],&V[i]);
+	T1[U[i]].pb(V[i]);T2[V[i]].pb(U[i]);
+    }
+    dfs1(1);dfs2(n);
+    //for (int i=1;i<=n;i++) cout<<M1[i]<<" ";cout<<endl;
+    //for (int i=1;i<=n;i++) cout<<M2[i]<<" ";cout<<endl;
+    for (int i=1;i<=m;i++)
+	if (M1[U[i]]&&M1[V[i]]&&M2[U[i]]&&M2[V[i]]){
+	    G[U[i]].pb(mp(V[i],-1));
+	    G[V[i]].pb(mp(U[i],2));
+	}
+
+    memset(Dst,127,sizeof(Dst));Dst[n]=0;Qu.push(n);inq[n]=1;++vis[n];
+    while (!Qu.empty()){
+	int u=Qu.front();Qu.pop();
+	for (int i=0;i<G[u].size();i++){
+	    int v=G[u][i].ft;
+	    if (Dst[v]>Dst[u]+G[u][i].sd){
+		Dst[v]=Dst[u]+G[u][i].sd;++vis[v];
+		if (v>=n){
+		    puts("No");return 0;
+		}
+		if (!inq[v]){
+		    Qu.push(v);inq[v]=1;
+		}
+	    }
+	}
+	inq[u]=0;
+    }
+
+    puts("Yes");
+    for (int i=1;i<=m;i++)
+	if (M1[U[i]]&&M1[V[i]]&&M2[U[i]]&&M2[V[i]]) printf("%d\n",Dst[U[i]]-Dst[V[i]]);
+	else puts("1");
+    return 0;
+}
+void dfs1(int u){
+    M1[u]=1;for (int i=0;i<T1[u].size();i++) if (!M1[T1[u][i]]) dfs1(T1[u][i]);
+    return;
+}
+void dfs2(int u){
+    M2[u]=1;for (int i=0;i<T2[u].size();i++) if (!M2[T2[u][i]]) dfs2(T2[u][i]);
+    return;
+}
---
+++ Practice/2019.7.8/LOJ2162.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+const int maxN=101000;
+const int maxM=1010000<<1;
+
+int n,m;
+int ecnt=-1,Hd[maxN],Nt[maxM],V[maxM],Dg[maxN];
+int vis[maxN],vism[maxM];
+int eucnt,Eular[maxM],top,St[maxM],inst[maxN];
+int Ans[maxM],Lf[maxN],Rh[maxN];
+
+void Add_Edge(int u,int v);
+void dfs(int u);
+int main(){
+    //freopen("in","r",stdin);freopen("out","w",stdout);
+    memset(Hd,-1,sizeof(Hd));
+    scanf("%d%d",&n,&m);
+    for (int i=1;i<=m;i++){
+	int u,v,a,b;scanf("%d%d%d%d",&u,&v,&a,&b);
+	if (a!=b) Add_Edge(u,v);
+    }
+    for (int i=1;i<=n;i++)
+	if (Dg[i]&1){
+	    puts("NIE");return 0;
+	}
+
+    for (int i=1;i<=n;i++) if (!vis[i]) dfs(i);
+
+    //for (int i=1;i<=eucnt;i++) cout<<Eular[i]<<" ";cout<<endl;
+    //cerr<<eucnt<<endl;
+    reverse(&Eular[1],&Eular[eucnt+1]);
+    int cnt=0,seqcnt=0;
+    for (int i=1;i<=eucnt;i++)
+	if (inst[Eular[i]]==1){
+	    ++cnt;Lf[cnt]=seqcnt+1;Ans[++seqcnt]=Eular[i];
+	    while (top&&St[top]!=Eular[i]) --inst[St[top]],Ans[++seqcnt]=St[top--];
+	    Ans[++seqcnt]=Eular[i];Rh[cnt]=seqcnt;
+	}
+	else{
+	    ++inst[Eular[i]];St[++top]=Eular[i];
+	}
+
+    printf("%d\n",cnt);
+    for (int i=1;i<=cnt;i++){
+	printf("%d ",Rh[i]-Lf[i]);
+	for (int j=Lf[i];j<=Rh[i];j++) printf("%d ",Ans[j]);
+	puts("");
+    }
+
+    return 0;
+}
+void Add_Edge(int u,int v){
+    Nt[++ecnt]=Hd[u];Hd[u]=ecnt;V[ecnt]=v;
+    Nt[++ecnt]=Hd[v];Hd[v]=ecnt;V[ecnt]=u;
+    ++Dg[u];++Dg[v];return;
+}
+void dfs(int u){
+    vis[u]=1;
+    for (int &i=Hd[u];i!=-1;i=Nt[i]){
+	if (vism[i]||vism[i^1]) continue;
+	vism[i]=vism[i^1]=1;dfs(V[i]);
+    }
+    Eular[++eucnt]=u;
+    return;
+}
